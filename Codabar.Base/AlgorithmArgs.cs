392b71f5006105d1e4face3290f67f7252bde9b7
misikdmitriy <bastardd666@gmail.com>
2018-05-07 13:42:50 +0300

    change start args description

--- Codabar.Base/AlgorithmArgs.cs
+++ Codabar.Base/AlgorithmArgs.cs
@@ -4,22 +4,22 @@ namespace Codabar.Base
 {
     public class AlgorithmArgs
     {
-        [Option('o', "Outpub PNG", Required = true, HelpText = "Output file")]
+        [Option('o', "output", Required = true, HelpText = "Output file")]
         public string OutputPath { get; set; }
 
-        [Option('i', "Input", Required = true, HelpText = "Input text")]
+        [Option('i', "input", Required = true, HelpText = "Input text")]
         public string Text { get; set; }
 
-        [Option('s', "Start symbol", Required = false, HelpText = "Start symbol for codabar", Default = 'a')]
+        [Option('s', "start", Required = false, HelpText = "Start symbol for codabar", Default = 'a')]
         public char StartSymbol { get; set; }
 
-        [Option('e', "End symbol", Required = false, HelpText = "End symbol for codabar", Default = 'b')]
+        [Option('e', "end", Required = false, HelpText = "End symbol for codabar", Default = 'b')]
         public char EndSymbol { get; set; }
 
-        [Option('w', "Line width", Required = false, HelpText = "Line width", Default = 5)]
+        [Option('w', "width", Required = false, HelpText = "Line width", Default = 5)]
         public int LineWidth { get; set; }
 
-        [Option('h', "Line height", Required = false, HelpText = "Line height", Default = 150)]
+        [Option('h', "height", Required = false, HelpText = "Line height", Default = 150)]
         public int LineHeight { get; set; }
     }
 }
